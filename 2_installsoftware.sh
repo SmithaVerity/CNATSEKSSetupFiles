587fd74b04b1230d68bd177c3b0188c5d276cfc8
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2024-06-04 17:32:52 +0530

    Update 2_installsoftware.sh

--- 2_installsoftware.sh
+++ 2_installsoftware.sh
@@ -1,1 +1,13 @@
-dsfgdf
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv -v ./kubectl /usr/local/bin 
+kubectl version
+
+
+curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv -v /tmp/eksctl /usr/local/bin
+eksctl version
+
+
+curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
+helm version 
