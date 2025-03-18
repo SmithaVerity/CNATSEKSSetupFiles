0ac92506c35e10455123ba0513f5f440c1e1f943
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-18 10:14:57 +0530

    Update 4_downloadcode.sh

--- 4_downloadcode.sh
+++ 4_downloadcode.sh
@@ -1,3 +1,4 @@
+cd ..
 git clone https://github.com/aws-containers/ecsdemo-frontend.git
 git clone https://github.com/aws-containers/ecsdemo-nodejs.git
 git clone https://github.com/aws-containers/ecsdemo-crystal.git
