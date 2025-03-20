d9f2c77b503de02bcd639802da697609b63a954d
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-20 12:20:02 +0530

    Update 1_setupInfra.sh

--- 1_setupInfra.sh
+++ 1_setupInfra.sh
@@ -20,13 +20,13 @@ export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 export AWS_REGION=ap-south-1
 export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION))
 test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo AWS_REGION is not set
-echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
-echo "export AWS_REGION=ap-south-1" | tee -a ~/.bash_profile
-echo "export AZS=(${AZS[@]})" | tee -a ~/.bash_profile
+echo "export ACCOUNT_ID=${ACCOUNT_ID}"
+echo "export AWS_REGION=ap-south-1" 
+echo "export AZS=(${AZS[@]})"
 aws configure set default.region ${AWS_REGION}
 aws configure get default.region
 aws kms create-alias --alias-name alias/ekssetup --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
 export MASTER_ARN=$(aws kms describe-key --key-id alias/ekssetup --query KeyMetadata.Arn --output text)
-echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
+echo "export MASTER_ARN=${MASTER_ARN}" 
 
 
