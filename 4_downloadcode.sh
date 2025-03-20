9a4b77b08480982c8c66863ea50a17665a313d1e
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-20 11:43:17 +0530

    Update 4_downloadcode.sh

--- 4_downloadcode.sh
+++ 4_downloadcode.sh
@@ -5,7 +5,15 @@ git clone https://github.com/aws-containers/ecsdemo-crystal.git
 aws kms create-alias --alias-name alias/ekssetup --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
 export MASTER_ARN=$(aws kms describe-key --key-id alias/ekssetup --query KeyMetadata.Arn --output text)
 echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
-
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+export AWS_REGION=ap-south-1
+export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION))
+test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo AWS_REGION is not set
+echo "export ACCOUNT_ID=${ACCOUNT_ID}" | tee -a ~/.bash_profile
+echo "export AWS_REGION=ap-south-1" | tee -a ~/.bash_profile
+echo "export AZS=(${AZS[@]})" | tee -a ~/.bash_profile
+aws configure set default.region ${AWS_REGION}
+aws configure get default.region
 
 echo account=${ACCOUNT_ID}
 echo region=${AWS_REGION}
