04d06d2bfc3e7cd0ac9438c6f0114b33f570d363
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-03-20 13:15:11 +0530

    Update 1_setupInfra.sh

--- 1_setupInfra.sh
+++ 1_setupInfra.sh
@@ -16,16 +16,14 @@ git clone https://github.com/aws-containers/ecsdemo-frontend.git
 git clone https://github.com/aws-containers/ecsdemo-nodejs.git
 git clone https://github.com/aws-containers/ecsdemo-crystal.git
 
-export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
-export AWS_REGION=ap-south-1
-export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION))
-test -n "$AWS_REGION" && echo AWS_REGION is "$AWS_REGION" || echo AWS_REGION is not set
-echo "export ACCOUNT_ID=${ACCOUNT_ID}"
-echo "export AWS_REGION=ap-south-1" 
-echo "export AZS=(${AZS[@]})"
-
 aws kms create-alias --alias-name alias/ekssetup --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
 export MASTER_ARN=$(aws kms describe-key --key-id alias/ekssetup --query KeyMetadata.Arn --output text)
 echo "export MASTER_ARN=${MASTER_ARN}" 
+export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
+export AWS_REGION=ap-south-1
+export AZS=($(aws ec2 describe-availability-zones --query 'AvailabilityZones[].ZoneName' --output text --region $AWS_REGION))
 
+echo region=${AWS_REGION}
+echo az=${AZS[0]}
+echo key=${MASTER_ARN}
 
