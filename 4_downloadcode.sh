2d1e963ef86c67e8e19e7d343b8055abda0e340a
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-06-16 11:44:58 +0530

    Update 4_downloadcode.sh

--- 4_downloadcode.sh
+++ 4_downloadcode.sh
@@ -2,11 +2,4 @@
 git clone https://github.com/aws-containers/ecsdemo-frontend.git
 git clone https://github.com/aws-containers/ecsdemo-nodejs.git
 git clone https://github.com/aws-containers/ecsdemo-crystal.git
-aws kms create-alias --alias-name alias/ekssetup --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
-export MASTER_ARN=$(aws kms describe-key --key-id alias/ekssetup --query KeyMetadata.Arn --output text)
-echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
 
-echo account=${ACCOUNT_ID}
-echo region=${AWS_REGION}
-echo az=${AZS[0]}
-echo key=${MASTER_ARN}
