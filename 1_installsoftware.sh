57cf4400686ba768a10b51329ab2768803f3662f
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-06-16 20:41:27 +0530

    Update 1_installsoftware.sh

--- 1_installsoftware.sh
+++ 1_installsoftware.sh
@@ -1,32 +1,16 @@
 sudo yum -y install python-pip && sudo yum install firewalld -y
 pip3 install --user --upgrade boto3
-export instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-echo instance_id=${instance_id}
 
-sudo curl --silent --location -o /usr/local/bin/kubectl https://s3.us-west-2.amazonaws.com/amazon-eks/1.21.5/2022-01-21/bin/linux/amd64/kubectl
-sudo chmod +x /usr/local/bin/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv -v ./kubectl /usr/local/bin 
+kubectl version
 
-sudo yum -y install jq gettext bash-completion
 
-for command in kubectl jq envsubst aws
-  do
-    which $command &>/dev/null && echo "$command in path" || echo "$command NOT FOUND"
-  done
-kubectl completion bash >>  ~/.bash_completion
-. /etc/profile.d/bash_completion.sh
-. ~/.bash_completion
-echo 'export LBC_VERSION="v2.4.1"' >>  ~/.bash_profile
-echo 'export LBC_CHART_VERSION="1.4.1"' >>  ~/.bash_profile
-.  ~/.bash_profile
-curl --silent --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+curl --location "https://github.com/weaveworks/eksctl/releases/latest/download/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv -v /tmp/eksctl /usr/local/bin
 eksctl version
-eksctl completion bash >> ~/.bash_completion
-. /etc/profile.d/bash_completion.sh
-. ~/.bash_completion
+
+
 curl -sSL https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 helm version 
-helm completion bash >> ~/.bash_completion
-. /etc/profile.d/bash_completion.sh
-. ~/.bash_completion
-source <(helm completion bash)
