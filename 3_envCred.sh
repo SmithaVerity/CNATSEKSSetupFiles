a5a141466637b18e44beb607ba8dd0f3aa0e154d
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-06-16 21:16:28 +0530

    Update 3_envCred.sh

--- 3_envCred.sh
+++ 3_envCred.sh
@@ -1,4 +1,6 @@
+
 export instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
+echo instance_id=${instance_id}
 
 export ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 export AWS_REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r '.region')
@@ -9,7 +11,7 @@ echo "export AWS_REGION=${AWS_REGION}" | tee -a ~/.bash_profile
 echo "export AZS=(${AZS[@]})" | tee -a ~/.bash_profile
 aws configure set default.region ${AWS_REGION}
 aws configure get default.region
-aws kms create-alias --alias-name alias/ekssetup --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
+aws kms create-alias --alias-name alias/ekssetup1 --target-key-id $(aws kms create-key --query KeyMetadata.Arn --output text)
 export MASTER_ARN=$(aws kms describe-key --key-id alias/ekssetup --query KeyMetadata.Arn --output text)
 echo "export MASTER_ARN=${MASTER_ARN}" | tee -a ~/.bash_profile
 
