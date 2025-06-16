23878512e50489d747e6a8455d8edee2d6052151
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-06-16 20:51:41 +0530

    Update 1_installsoftware.sh

--- 1_installsoftware.sh
+++ 1_installsoftware.sh
@@ -4,6 +4,10 @@ pip3 install --user --upgrade boto3
 export instance_id=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
 echo instance_id=${instance_id}
 
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv -v ./kubectl /usr/local/bin 
