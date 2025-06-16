6e63ae34d1429719e61f34eb56293d5560fad2b6
Smitha Menon <98273657+SmithaVerity@users.noreply.github.com>
2025-06-16 22:45:29 +0530

    Update 1_installsoftware.sh

--- 1_installsoftware.sh
+++ 1_installsoftware.sh
@@ -5,7 +5,7 @@ curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip
 unzip awscliv2.zip
 sudo ./aws/install
 
-curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.29.0/2024-01-04/bin/linux/amd64/kubectl
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.33.0/2025-05-01/bin/linux/amd64/kubectl
 chmod +x ./kubectl
 sudo mv -v ./kubectl /usr/local/bin 
 kubectl version
